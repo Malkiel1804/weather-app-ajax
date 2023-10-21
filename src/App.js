582cc22d462b1eaa0f6f54ab7b97026fa1c10962
Malkiel1804 <135064860+Malkiel1804@users.noreply.github.com>
2023-10-21 11:30:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
+        <h2>
+          <strong>Hello my name is Lara</strong>
+        </h2>
         <WeatherList />
       </header>
     </div>
